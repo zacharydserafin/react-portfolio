e5897484cabd551c1d1ca5971edb398afc64c9e7
zacharydserafin <zacharydserafin@gmail.com>
2023-06-16 18:56:13 -0500

    Added Logos and Links to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,15 @@ import React from 'react';
 export default function Footer() {
   return (
     <div>
-      <h2>Footer</h2>
+      <a href="https://github.com/zacharydserafin">
+        <img src="/assets/icons/github.png" alt="GitHub Logo"/>
+      </a>
+      <a href="https://www.linkedin.com/in/zachary-serafin-34154827b/">
+        <img src="/assets/icons/linkedin.png" alt="LinkedIn Logo"/>
+      </a>
+      <a href="https://twitter.com/ZacharySerafin">
+        <img src="/assets/icons/twitter.png" alt="Twitter Logo"/>
+      </a>
     </div>
   );
 }
