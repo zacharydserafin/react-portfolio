79a195b6d09052c26a8f3301586b3375c30ac135
zacharydserafin <zacharydserafin@gmail.com>
2023-06-20 17:20:11 -0500

    Formatted About Page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,20 +3,20 @@ import '../../styles/About.css';
 
 export default function About() {
   const bio = `Zachary D. Serafin is a web developer from Stevens Point, Wisconsin. As an avid lover of music and technology, he channels his creativity and ingenuity into everything he does. Whether it be coding, music production, or any one of the numerous creative hobbies he spends his time on, Zachary always strives to make something unique and fresh. Under the "Portfolio" tab you'll find examples of his work, and if you want your web development team to have a creative powerhouse on board, make sure to reach out via the "Contact" section.`;
-
-  // temporary inline styles
-  const styles = {
-    image: {
-      height: 'auto',
-      width: '20%'
-    }
-  }
   
   return (
-    <div>
-      <h2>About Me</h2>
-      <img src="/assets/images/profile-image.jpg" style={styles.image} alt="Zachary D Serafin"></img>
-      <p>{bio}</p>
+    <div className="card mb-3 text-start about-card">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src="/assets/images/profile-image.jpg" className="img-fluid rounded-start" alt="Zachary D Serafin"/>
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h3 className="card-title">About Me</h3>
+            <p className="card-text">{bio}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
